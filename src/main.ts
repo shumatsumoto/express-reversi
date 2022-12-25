32eccf66c3f8017fe2f5c5bd1f885a6a6c981ee3
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-25 15:24:58 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/api/hello", async (req, res) => {
+  res.json({
+    message: "hello, shu",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Reversi app started: http://localhost:${PORT}`);
+});
